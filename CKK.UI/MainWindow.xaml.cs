77f9d9a54948ec0a06d7d3f8bf01883feecd7bae
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-08-02 14:50:01 -0600

    feat: Added XAML gridfrom Corey tutorial

---
+++ CKK.UI/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CKK.Logic.Interfaces;
+using CKK.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CKK.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IStore Store;
+
+        public MainWindow()
+        {
+            Store = new Store();
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Priviet { firstNameText.Text }");
+        }
+    }
+}
